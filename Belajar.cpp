74c56550892d211d41ddd079b43babcc9203e9db
Andrian Suherman <120797811+AndriHerman@users.noreply.github.com>
2022-12-21 16:43:00 +0100

    Belajar Bahasa Pemograman C++

---
+++ Belajar.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std; 
+int main(int argc, char*argv[])
+{
+    char nama[10];
+    cout<<"masukkan nama anda :";
+    cin>>nama;
+    cout<<endl;
+    cout<<"Hello"<<nama<<endl;
+    cout<<"Saya Belajar C++";
+    cout<<"\n\n";
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
